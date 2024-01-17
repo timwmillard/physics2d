887e9cf4c6f35a01915e29a97c55d8dfe3981db1
Tim Millard <tim@rapidpaylegal.com>
2024-01-18 00:01:53 +1100

    shape collision restructure

--- src/physics2d.h
+++ src/physics2d.h
@@ -18,6 +18,22 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+physics2d.h is a simple physics 2D library for programming games and
+simulations.
+
+This is a single-header-file library that provides easy-to-use
+
+
+Usage:
+To use this library, copy physics2d.h into project.
+This include this in one C files:
+
+   #define PHYSICS2D_IMPLEMENTATION
+   #include "physics2d.h"
+
 */
 
 #ifndef PHYSICS2D_H
@@ -27,9 +43,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
-#include <float.h>
-
-#include "stb_ds.h"
+#include <stdarg.h>
 
 static inline double map(const double value,
         const double start1, const double stop1,
@@ -66,7 +80,7 @@ extern double noise3(double x, double y, double z);
 
 
 /**
- * 2D Vector function
+ * 2D Vector functions
  * 
  */
 typedef struct Vec2 {
@@ -164,17 +178,21 @@ static inline Vec2 vec2_limit(const Vec2 v, double max)
 /**
  * Vec2 TODO Funcs
 
-
-// Limits a vector's magnitude to a maximum value. 
+// Limits a vector's magnitude to a maximum value.
 static inline Vec2 vec2_limit(const Vec2 v, double m);
 
-// Calculates the angle a 2D vector makes with the positive x-axis. Angles increase in the clockwise direction.p
+// Calculates the angle a 2D vector makes with the positive x-axis. Angles
+// increase in the clockwise direction.p
 static inline double vec2_heading(const Vec2 v);
 
-// Rotates a 2D vector to a specific angle without changing its magnitude. By convention, the positive x-axis has an angle of 0. Angles increase in the clockwise direction.
+// Rotates a 2D vector to a specific angle without changing its magnitude. By
+// convention, the positive x-axis has an angle of 0. Angles increase in the
+// clockwise direction.
 static inline Vec2 vec2_set_heading(const Vec2 v, double angle);
 
-// Rotates a 2D vector by an angle without changing its magnitude. By convention, the positive x-axis has an angle of 0. Angles increase in the clockwise direction.
+// Rotates a 2D vector by an angle without changing its magnitude. By
+// convention, the positive x-axis has an angle of 0. Angles increase in the
+// clockwise direction.
 static inline Vec2 vec2_rotate(const Vec2 v, double angle);
 
 // Returns the angle between two vectors. The angle returned are signed.
@@ -195,8 +213,6 @@ static inline void print_vec2(Vec2 v)
 }
 
 
-
-
 /**
  * Body is a physics body that lives in an enviroment.
  * 
@@ -215,9 +231,9 @@ typedef struct Body {
     // Mass
     double mass;
 
+    // Maximum speed
     double max_speed;
 
-    Vec2 *shapes;
 } Body;
 
 extern Body *body_alloc();
@@ -228,8 +244,202 @@ extern void body_apply_gravity(Body *body, const Vec2 gravity);
 extern void body_update(Body *body, const double dt);
 extern Vec2 body_momentum(Body *body);
 
+
+/************
+ * Shapes
+ * 
+ *
+ */
+
+// Point
+typedef Vec2 Point;
+
+
+// Line
+typedef struct Line {
+    Vec2 v1;
+    Vec2 v2;
+} Line;
+
+static inline Line line(double x1, double y1, double x2, double y2)
+{
+    Line l = {
+        .v1 = vec2(x1, y1),
+        .v2 = vec2(x2, y2),
+    };
+    return l;
+}
+
+static inline Line linev(Vec2 v1, Vec2 v2)
+{
+    Line l = {
+        .v1 = v1,
+        .v2 = v2,
+    };
+    return l;
+}
+
+
+// Circle
+typedef struct Circle {
+    Vec2 center;
+    double radius;
+} Circle;
+
+
+// Triangle
+typedef struct Triangle {
+    Vec2 v1;
+    Vec2 v2;
+    Vec2 v3;
+} Triangle;
+
+
+// Polygon
+typedef struct Poly {
+    int n;
+    Vec2 *v;
+} Poly;
+
+
+// Rectangle
+typedef struct Rect {
+    Vec2 pos;
+    double width;
+    double height;
+} Rect;
+
+
+// Shape Type
+typedef enum ShapeType {
+    POINT,
+    LINE,
+    CIRCLE,
+    RECT,
+    TRIANGLE,
+    POLY,
+} ShapeType;
+
+// Shape is a general shape.
+typedef struct Shape {
+    ShapeType type;
+    union {
+        Point point;
+        Line line;
+        Circle circle;
+        Rect rect;
+        Triangle triangle;
+        Poly poly;
+    };
+} Shape;
+
+
+static inline Shape point(const double x, const double y)
+{
+    Point p = {x, y};
+    Shape s = {
+        .type = POINT,
+        .point = p,
+    };
+    return s;
+}
+
+static inline Shape circlev(Vec2 center, double radius)
+{
+    Circle c = {
+        .center = center,
+        .radius = radius,
+    };
+    Shape s = {
+        .type = CIRCLE,
+        .circle = c,
+    };
+    return s;
+}
+
+static inline Shape circle(double x, double y, double radius)
+{
+    return circlev(vec2(x,y), radius);
+}
+
+static inline Shape rectv(const Vec2 pos, const double width, const double height)
+{
+    Rect r= {
+        .pos = pos,
+        .width = width,
+        .height = height,
+    };
+    Shape s = {
+        .type = RECT,
+        .rect = r,
+    };
+    return s;
+}
+
+static inline Shape rect(const double x, const double y, const double width, const double height)
+{
+    return rectv(vec2(x,y), width, height);
+}
+
+static inline Shape trianglev(Vec2 v1, Vec2 v2, Vec2 v3)
+{
+    Triangle t = {
+        .v1 = v1,
+        .v2 = v2,
+        .v3 = v3,
+    };
+    Shape s = {
+        .type = TRIANGLE,
+        .triangle = t,
+    };
+    return s;
+}
+
+static inline Shape triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+    return trianglev(vec2(x1,y1), vec2(x2,y2), vec2(x3,y3));
+}
+
+static inline Shape poly(int n, Vec2 v1, ...)
+{
+    Vec2 *v = (Vec2*) malloc(sizeof(Vec2) * n);
+
+    va_list args;
+    va_start(args, v1);
+    for (int i; i<n; i++) {
+        Vec2 va = va_arg(args, Vec2);
+        v[i] = va;
+    }
+    va_end(args);
+
+    Poly p = {
+        .n = n,
+        .v = v,
+    };
+    Shape s = {
+        .type = POLY,
+        .poly = p,
+    };
+    return s;
+}
+
+static inline void poly_free(Poly p)
+{
+    free(p.v);
+}
+
+// Shape free can be called on any shape.
+// Only a allocated poly will be freed.
+// All other shapes will safely be ignored.
+static inline void shape_free(Shape s)
+{
+    if (s.type == POLY) poly_free(s.poly);
+}
+
 #endif // End PHYSICS2D_H
 
+
+/******************************************************************************/
 
 
 #define PHYSICS2D_IMPLEMENTATION
@@ -238,6 +448,13 @@ extern Vec2 body_momentum(Body *body);
 
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
+
+
+/**********************************************
+ *
+ * Body
+ *
+ **********************************************/
 
 extern Body *body_alloc()
 {
@@ -343,158 +560,159 @@ void destroy_noise();
  *
  * Shapes
  *
- */
+ **********************************************/
 
 
-
-/**
- * Point
- * 
- */
-typedef struct Point {
-    Vec2 pos;
-} Point;
+// https://happycoding.io/tutorials/processing/collision-detection
+// https://www.jeffreythompson.org/collision-detection/index.php
 
 /**
- * Line
+ * Area functions
  * 
  */
-typedef struct Line {
-    Vec2 v1;
-    Vec2 v2;
-} Line;
 
+double point_area(Point p)
+{
+    return 1;
+}
 
-/**
- * Circle
- * 
- */
-typedef struct Circle {
-    Vec2 center;
-    double radius;
-} Circle;
+double line_area(Line l)
+{
+    return 0;
+}
 
-/**
- * Ellipse
- * 
- */
-typedef struct Ellipse {
-    Vec2 center;
-    double radius_x;
-    double radius_y;
-} Ellipse;
-
-/**
- * Triangle
- * 
- */
-typedef struct Triangle {
-    Vec2 v1;
-    Vec2 v2;
-    Vec2 v3;
-} Triangle;
-
-/**
- * Poly
- * 
- */
-typedef struct Poly {
-    Vec2 *v;
-} Poly;
-
-/**
- * Rectangle
- * 
- */
-typedef struct Rect {
-    Vec2 pos;
-    double width;
-    double height;
-} Rect;
-
-
-
-double point_area(Point p) { return 1; }
-bool point_collide(Point p1, Point p2) { return vec2_equal(p1.pos, p2.pos); }
-bool point_collide_line(Point p, Line l);
-bool point_collide_circle(Point p, Circle c);
-bool point_collide_ellipse(Point p, Ellipse e);
-bool point_collide_triangle(Point p, Triangle t);
-bool point_collide_rect(Point p, Rect r);
-bool point_collide_poly(Point p, Poly poly);
-
-double line_area(Line l) { return 0; }
-bool line_collide(Line l1, Line l2);
-bool line_collide_circle(Line l, Circle c);
-bool line_collide_ellipse(Line l, Ellipse e);
-bool line_collide_triangle(Line l, Triangle t);
-bool line_collide_rect(Line l, Rect r);
-bool line_collide_poly(Line l, Poly poly);
-
-double circle_area(Circle p) {
+double circle_area(Circle p)
+{
     return M_PI * p.radius  * p.radius;
 }
 
-bool circle_collide(Circle c1, Circle c2);
-bool circle_collide_line(Circle c, Line l);
-bool circle_collide_point(Circle c, Point p);
-bool circle_collide_ellipse(Circle p, Ellipse e);
-bool circle_collide_triangle(Circle p, Triangle t);
-bool circle_collide_rect(Circle p, Rect r);
-bool circle_collide_poly(Circle p, Poly poly);
+double triangle_area(Triangle t)
+{
+    return -1; // TODO
+}
 
-typedef enum ShapeType {
-    POINT,
-    LINE,
-    CIRCLE,
-    ELLIPSE,
-    RECT,
-    TRIANGLE,
-    POLY,
-} ShapeType;
+double rect_area(Rect r)
+{
+    return -1; // TODO
+}
 
-typedef struct Shape {
-    ShapeType type;
-    union {
-        Point point;
-        Line line;
-        Circle circle;
-        Ellipse ellipse;
-        Rect rect;
-        Triangle triangle;
-        Poly poly;
-    };
-} Shape;
-
-bool shape_collide(Shape s1, Shape s2);
+double poly_area(Poly p)
+{
+    return -1; // TODO
+}
 
 double shape_area(Shape s) {
     switch (s.type) {
         case POINT: return point_area(s.point); break;
         case LINE: return line_area(s.line); break;
         case CIRCLE: return circle_area(s.circle); break;
-        case ELLIPSE: return point_area(s.point); break;
-        case TRIANGLE: return point_area(s.point); break;
-        case RECT: return point_area(s.point); break;
-        case POLY: return point_area(s.point); break;
+        case TRIANGLE: return triangle_area(s.triangle); break;
+        case RECT: return rect_area(s.rect); break;
+        case POLY: return poly_area(s.poly); break;
     }
+    return false;
+}
+
+/**
+ * Collision functions
+ * 
+ */
+
+bool point_collide(Point p1, Point p2)
+{
+    int p1x = p1.x;
+    int p1y = p1.y;
+    int p2x = p2.x;
+    int p2y = p2.y;
+    return p1x == p2x && p1y == p2y;
+}
+bool point_collide_line(Point p, Line l) { return false; }
+bool point_collide_circle(Point p, Circle c) { return false; }
+bool point_collide_triangle(Point p, Triangle t) { return false; }
+bool point_collide_rect(Point p, Rect r) { return false; }
+bool point_collide_poly(Point p, Poly poly) { return false; }
+
+bool line_collide(Line l1, Line l2) { return false; }
+bool line_collide_point(Line l, Point p) { return false; }
+bool line_collide_circle(Line l, Circle c) { return false; }
+bool line_collide_triangle(Line l, Triangle t) { return false; }
+bool line_collide_rect(Line l, Rect r) { return false; }
+bool line_collide_poly(Line l, Poly poly) { return false; }
+
+bool circle_collide(Circle c1, Circle c2) { return false; }
+bool circle_collide_line(Circle c, Line l) { return false; }
+bool circle_collide_point(Circle c, Point p) { return false; }
+bool circle_collide_triangle(Circle p, Triangle t) { return false; }
+bool circle_collide_rect(Circle p, Rect r) { return false; }
+bool circle_collide_poly(Circle p, Poly poly) { return false; }
+
+bool rect_collide(Rect r1, Rect r2) { return false; }
+bool rect_collide_point(Rect r, Point p) { return false; }
+bool rect_collide_line(Rect r, Line l) { return false; }
+bool rect_collide_circle(Rect r, Circle c) { return false; }
+bool rect_collide_triangle(Rect r, Triangle t) { return false; }
+bool rect_collide_poly(Rect r, Poly poly) { return false; }
+
+bool triangle_collide(Triangle t1, Triangle t2) { return false; }
+bool triangle_collide_point(Triangle t, Point p) { return false; }
+bool triangle_collide_line(Triangle t, Line l) { return false; }
+bool triangle_collide_circle(Triangle t, Circle c) { return false; }
+bool triangle_collide_rect(Triangle t, Rect r) { return false; }
+bool triangle_collide_poly(Triangle t, Poly poly) { return false; }
+
+bool poly_collide(Poly p1, Poly p2) { return false; }
+bool poly_collide_point(Poly poly, Point point) { return false; }
+bool poly_collide_line(Poly p, Line l) { return false; }
+bool poly_collide_circle(Poly p, Circle c) { return false; }
+bool poly_collide_rect(Poly p, Rect r) { return false; }
+bool poly_collide_triangle(Poly p, Triangle poly) { return false; }
+
+bool shape_collide(Shape s1, Shape s2)
+{
+    if (s1.type == POINT && s2.type == POINT) return point_collide(s1.point, s2.point);
+    if (s1.type == POINT && s2.type == LINE) return point_collide_line(s1.point, s2.line);
+    if (s1.type == POINT && s2.type == CIRCLE) return point_collide_circle(s1.point, s2.circle);
+    if (s1.type == POINT && s2.type == TRIANGLE) return point_collide_triangle(s1.point, s2.triangle);
+    if (s1.type == POINT && s2.type == RECT) return point_collide_rect(s1.point, s2.rect);
+    if (s1.type == POINT && s2.type == POLY) return point_collide_poly(s1.point, s2.poly);
+
+    if (s1.type == LINE && s2.type == LINE) return line_collide(s1.line, s2.line);
+    if (s1.type == LINE && s2.type == POINT) return line_collide_point(s1.line, s2.point);
+    if (s1.type == LINE && s2.type == CIRCLE) return line_collide_circle(s1.line, s2.circle);
+    if (s1.type == LINE && s2.type == TRIANGLE) return line_collide_triangle(s1.line, s2.triangle);
+    if (s1.type == LINE && s2.type == RECT) return line_collide_rect(s1.line, s2.rect);
+    if (s1.type == LINE && s2.type == POLY) return line_collide_poly(s1.line, s2.poly);
+
+    if (s1.type == CIRCLE && s2.type == LINE) return circle_collide_line(s1.circle, s2.line);
+    if (s1.type == CIRCLE && s2.type == POINT) return circle_collide_point(s1.circle, s2.point);
+    if (s1.type == CIRCLE && s2.type == CIRCLE) return circle_collide(s1.circle, s2.circle);
+    if (s1.type == CIRCLE && s2.type == TRIANGLE) return circle_collide_triangle(s1.circle, s2.triangle);
+    if (s1.type == CIRCLE && s2.type == RECT) return circle_collide_rect(s1.circle, s2.rect);
+    if (s1.type == CIRCLE && s2.type == POLY) return circle_collide_poly(s1.circle, s2.poly);
+
+    if (s1.type == RECT && s2.type == LINE) return rect_collide_line(s1.rect, s2.line);
+    if (s1.type == RECT && s2.type == POINT) return rect_collide_point(s1.rect, s2.point);
+    if (s1.type == RECT && s2.type == CIRCLE) return rect_collide(s1.rect, s2.rect);
+    if (s1.type == RECT && s2.type == TRIANGLE) return rect_collide_triangle(s1.rect, s2.triangle);
+    if (s1.type == RECT && s2.type == RECT) return rect_collide(s1.rect, s2.rect);
+    if (s1.type == RECT && s2.type == POLY) return rect_collide_poly(s1.rect, s2.poly);
+
+    if (s1.type == TRIANGLE && s2.type == LINE) return triangle_collide_line(s1.triangle, s2.line);
+    if (s1.type == TRIANGLE && s2.type == POINT) return triangle_collide_point(s1.triangle, s2.point);
+    if (s1.type == TRIANGLE && s2.type == CIRCLE) return triangle_collide(s1.triangle, s2.triangle);
+    if (s1.type == TRIANGLE && s2.type == TRIANGLE) return triangle_collide(s1.triangle, s2.triangle);
+    if (s1.type == TRIANGLE && s2.type == RECT) return triangle_collide(s1.triangle, s2.triangle);
+    if (s1.type == TRIANGLE && s2.type == POLY) return triangle_collide_poly(s1.triangle, s2.poly);
+
     return false;
 }
 
 Vec2 shape_center(Shape s1, Shape s2);
 
-static inline Shape rect(const double x, const double y, const double width, const double height)
+// TODO: delete me, just for testing purposes
+static inline double kinamatic(double acc, double vel, double pos, double t)
 {
-    Rect r= {
-        .pos = vec2(x, y),
-        .width = width,
-        .height = height,
-    };
-    Shape s = {
-        .type = RECT,
-        .rect = r,
-    };
-    return s;
+    return 0.5f * acc * t * t * vel * t + pos;
 }
 
 #endif // End PHYSICS2D_IMPLEMENTATION
